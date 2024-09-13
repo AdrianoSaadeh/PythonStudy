358b785b672960aeec40337b4321977d47482c77
Adriano Saadeh <adriano.saadeh@sellersfunding.com>
2024-09-13 08:51:30 -0300

    ajustando terminal e codigo

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 
+
 def exibir_nome_do_programa():
     print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -10,25 +11,29 @@ def exibir_nome_do_programa():
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
 """)
 
+
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurantes')
     print('3. Ativar restaurantes')
     print('4. Sair\n')
 
+
 def finalizar_app():
     os.system('cls')
     print('Finalizando o app\n')
+
 
 def opcao_invalida():
     print('Opção Inválida!\n')
     input("Digite uma tecla para voltar ao menu principal.")
 
+
 def escolher_opcao():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
         # opcao_escolhida = int(opcao_escolhida)
-        
+
         if opcao_escolhida == 1:
             print('Cadastrar restaurante')
         elif opcao_escolhida == 2:
@@ -42,11 +47,13 @@ def escolher_opcao():
     except:
         opcao_invalida()
 
+
 def main():
     os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
-    
+
+
 if __name__ == '__main__':
     main()
